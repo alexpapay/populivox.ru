37bbf9cbebe68ca33b5c38d1a285c59fd3ac54f8
alexpapay <alexpapay@mail.ru>
2019-05-16 23:18:27 +0200

    @2 Modal windows created for Login and Register actions.

---
+++ PopuliVoxRu.View.Data/ViewModels/Identity/UserLoginViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PopuliVoxRu.View.Data.ViewModels.Identity
+{
+    public class UserLoginViewModel
+    {
+        [Required]
+        [Display(Name = "Почта")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+
+        [Display(Name = "Запомнить?")]
+        public bool RememberMe { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+}
--- PopuliVoxRu/wwwroot/js/site.js
+++ PopuliVoxRu/wwwroot/js/site.js
@@ -1,4 +1,53 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(function () {
 
-// Write your JavaScript code.
+    $("#registerModalCenter").validate({
+		rules: {
+			pName: {
+				required: true,
+				minlength: 8
+			},
+			action: "required"
+		},
+		messages: {
+			pName: {
+				required: "Please enter some data",
+				minlength: "Your data must be at least 8 characters"
+			},
+			action: "Please provide some data"
+		}
+	});
+});
+
+$(function () {
+    $("#loginModalCenter").validate({
+		rules: {
+			pName: {
+				required: true,
+				minlength: 8
+			},
+			action: "required"
+		},
+		messages: {
+			pName: {
+				required: "Please enter some data",
+				minlength: "Your data must be at least 8 characters"
+			},
+			action: "Please provide some data"
+		}
+	});
+});
+
+//// add the rule here
+//$.validator.addMethod("valueNotEquals", function (value, element, arg) {
+//	return arg !== value;
+//}, "Value must not equal arg.");
+
+//// configure your validation
+//$("form").validate({
+//	rules: {
+//		SelectName: { valueNotEquals: "default" }
+//	},
+//	messages: {
+//		SelectName: { valueNotEquals: "Please select an item!" }
+//	}
+//});
